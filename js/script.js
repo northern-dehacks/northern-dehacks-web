a3e6b01b2900d09b72b866a5f8abcdbcef6b4586
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-04-09 23:25:05 -0400

    material buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $('#newsletter-form').submit(function(e){
-    console.log('ok')
     e.preventDefault();
     $.ajax({
         url: 'http://localhost:8080/newsletter',
@@ -10,3 +9,29 @@ $('#newsletter-form').submit(function(e){
         }
     });
 });
+
+function createRipple(event) {
+    const button = event.currentTarget;
+  
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+  
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+    circle.classList.add("ripple");
+  
+    const ripple = button.getElementsByClassName("ripple")[0];
+  
+    if (ripple) {
+      ripple.remove();
+    }
+  
+    button.appendChild(circle);
+  }
+  
+  const buttons = document.getElementsByClassName("cta-btn");
+  for (const button of buttons) {
+    button.addEventListener("click", createRipple);
+  }

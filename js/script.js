9019e596aec159192bc3ddb5a4426338e6961347
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-04-09 23:55:32 -0400

    start footer

--- js/script.js
+++ js/script.js
@@ -5,33 +5,10 @@ $('#newsletter-form').submit(function(e){
         type: 'post',
         data:$('#newsletter-form').serialize(),
         success:function(){
-            alert('Success')
+            alert('Subscribed')
+        },
+        error: function(){
+          alert('Error')
         }
     });
 });
-
-function createRipple(event) {
-    const button = event.currentTarget;
-  
-    const circle = document.createElement("span");
-    const diameter = Math.max(button.clientWidth, button.clientHeight);
-    const radius = diameter / 2;
-  
-    circle.style.width = circle.style.height = `${diameter}px`;
-    circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-    circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
-    circle.classList.add("ripple");
-  
-    const ripple = button.getElementsByClassName("ripple")[0];
-  
-    if (ripple) {
-      ripple.remove();
-    }
-  
-    button.appendChild(circle);
-  }
-  
-  const buttons = document.getElementsByClassName("cta-btn");
-  for (const button of buttons) {
-    button.addEventListener("click", createRipple);
-  }

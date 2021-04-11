215635f8f7597dd8730cf34986e25f796d99b955
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-04-11 12:09:33 -0400

    email form fix

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-$('#newsletter-form').submit(function (_) {
+$('#newsletter-form').submit(function (e) {
+    e.preventDefault();
     $.ajax({
         url: 'https://northerndehacks-server.herokuapp.com/newsletter',
         type: 'post',
         data: $('#newsletter-form').serialize(),
-        success: function () {
+        success: function (_) {
             alert('Subscribed')
         },
-        error: function () {
+        error: function (_) {
             alert('Error')
         }
     });
 });
 
-$('#contact-form').submit(function (_) {
+$('#contact-form').submit(function (e) {
+    e.preventDefault();
     $.ajax({
         url: 'https://northerndehacks-server.herokuapp.com/contact',
         type: 'post',
         data: $('#contact-form').serialize(),
-        success: function () {
+        success: function (_) {
             alert('Message Sent')
         },
-        error: function () {
+        error: function (_) {
             alert('Error')
         }
     });

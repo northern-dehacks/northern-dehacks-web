a80bb898eaa3382a130b07c7e4541e8aecb3463c
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-04-10 20:54:33 -0400

    done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-$('#newsletter-form').submit(function(e){
-    e.preventDefault();
+$('#newsletter-form').submit(function (_) {
     $.ajax({
         url: 'http://localhost:8080/newsletter',
         type: 'post',
-        data:$('#newsletter-form').serialize(),
-        success:function(){
+        data: $('#newsletter-form').serialize(),
+        success: function () {
             alert('Subscribed')
         },
-        error: function(){
-          alert('Error')
+        error: function () {
+            alert('Error')
+        }
+    });
+});
+
+$('#contact-form').submit(function (_) {
+    $.ajax({
+        url: 'http://localhost:8080/contact',
+        type: 'post',
+        data: $('#contact-form').serialize(),
+        success: function () {
+            alert('Message Sent')
+        },
+        error: function () {
+            alert('Error')
         }
     });
 });
